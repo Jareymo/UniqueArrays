1f4e5cb63948eeab7ce2280f9c199f60eff30ec5
José Antonio Rey Morales <endlesssonata@gmail.com>
2017-03-08 00:24:44 +0100

    Update UArray.js

--- UArray.js
+++ UArray.js
@@ -1,3 +1,12 @@
+/* Turns arrays into arrays with Unique Values
+ *
+ * Use: Call the function, pass the array with duplicate values to the funcion and save the value in another variable.
+ * 
+ * In example: var uArray = arrayToUArray(oldArray);
+ * 
+ * By: José Rey
+ */
+
 function arrayToUArray(_array)
 {
     var _newArray = [];
